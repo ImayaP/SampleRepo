b44d44333c4e2eee2176b1c0d61455ee3fcfff3b
Purushothaman <imaya.purushothaman@gmail.com>
2018-08-27 12:22:53 +0530

    Adding project content

---
+++ SampleDemo/src/main/java/com/example/sample/SampleDemo/App.java
@@ -0,0 +1,17 @@
+package com.example.sample.SampleDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    /*public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }*/
+	public String displayHelloMessage(String message){
+		message = "hi";
+		return message;
+	}
+}
